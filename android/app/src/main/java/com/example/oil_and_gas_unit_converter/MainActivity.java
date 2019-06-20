3f1785f6e57f9d890cb5e56655796ed537b30480
herovickers <victoreronmosele@gmail.com>
2019-06-20 14:57:19 +0100

    Create project

---
+++ android/app/src/main/java/com/example/oil_and_gas_unit_converter/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.oil_and_gas_unit_converter;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
